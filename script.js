434719d4aee4cb4cc362d6db81f8d0aacd11cebb
zgoodman27 <zgoodman27@gmail.com>
2025-07-07 22:49:22 -0400

    finished comments

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ async function render(fi, si) {
     let data = await response.json()
     console.log(data)
     // Your code goes here
-    let firstCharacter = data[0];
-    let secondCharacter = data[1];
-    document.getElementById('firstImage').src = firstCharacter.image;
-    document.getElementById('secondImage').src = secondCharacter.image;
+    let firstCharacter = data[0]; //grab the first character's data
+    let secondCharacter = data[1]; //grab the second character's data
+    document.getElementById('firstImage').src = firstCharacter.image; //grab the first character's image
+    document.getElementById('secondImage').src = secondCharacter.image; //grab the second character's image
 }
 
 render(20, 35);
