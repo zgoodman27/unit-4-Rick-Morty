02553ee11a56cfc61cf1f15a52629d86107d9fdd
zgoodman27 <zgoodman27@gmail.com>
2025-07-07 22:46:23 -0400

    finished project prior to comments

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ async function render(fi, si) {
     let data = await response.json()
     console.log(data)
     // Your code goes here
+    let firstCharacter = data[0];
+    let secondCharacter = data[1];
+    document.getElementById('firstImage').src = firstCharacter.image;
+    document.getElementById('secondImage').src = secondCharacter.image;
 }
 
 render(20, 35);
